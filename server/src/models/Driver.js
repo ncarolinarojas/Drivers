43a38e612e23ba260ee52c7d55d14ae4c66573b1
ncarolinarojas <nataliarojasco22@gmail.com>
2023-12-05 23:07:01 -0500

    models

--- server/src/models/Driver.js
+++ server/src/models/Driver.js
@@ -4,9 +4,37 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('Driver', {
+    id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    forname: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    surname: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    nacionality: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }, 
+    birth: {
+      type: DataTypes.DATE
+    }
   });
 };
